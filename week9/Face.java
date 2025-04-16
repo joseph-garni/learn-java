e40b1c7e1ef2867467bcf64f7279f7e817d63da4
Joseph Garni <joseph.garni@student.manchester.ac.uk>
2025-04-16 18:22:46 +0100

    fixed minor buds in file

--- week9/Face.java
+++ week9/Face.java
@@ -16,11 +16,11 @@ import javafx.stage.Stage;
 
 // as all JavaFX programs run as application we have to extend the Application class
 
-public class Face extends Application
+public class Face extends Application{
 
     @Override 
-    public void start (Stage stage) // Application class requires you to code the start method
-    {                               // when start is called it is automatically sent an object of the Stage class -> which will be the main container for our graphic
+    public void start (Stage stage){ // Application class requires you to code the start method
+                                   // when start is called it is automatically sent an object of the Stage class -> which will be the main container for our graphic
         // first we create the main circle for the face
 
         Circle face = new Circle(100, 100, 75);
@@ -32,8 +32,8 @@ public class Face extends Application
         rightEye.setStroke(Color.RED);
 
         Circle leftEye = new Circle(110, 100, 6);
-        rightEye.setFill(Color.BLACK);
-        rightEye.setStroke(Color.RED);
+        leftEye.setFill(Color.BLACK);
+        leftEye.setStroke(Color.RED);
         
         // new arc instance created named mouth where we pass through the Arc method using the values in the method we're asking for
         Arc mouth = new Arc(120, 150, 40, 32.5, 0, -180); 
@@ -44,10 +44,10 @@ public class Face extends Application
         // Creating instance of text class called 'caption' -> our caption for our application, with its position x, y, and the text itself (the inputs of the method)
         Text caption = new Text(80, 220, "Joseph's Chapter 10 Intial Application!");
         caption.setFill(Color.BLACK);
-        caption.SetFont(Font.font("Verdana", 15));
+        caption.setFont(Font.font("Verdana", 15));
 
         // now we have created all our features, we want them to stay together as a group
-        Group root = new Group(face, rightEye, leftEye, mouth, text, caption);
+        Group root = new Group(face, rightEye, leftEye, mouth, caption);
 
         // we are using the convention of the first node we add to our scene as root
         // here we are using the constructor that allows us to set the size and height of the initial scene with the Scene class
