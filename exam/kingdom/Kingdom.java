840f7cf5d413c607e496b7e519b609a387ff254d
Joseph Garni <joseph.garni@student.manchester.ac.uk>
2025-04-29 17:19:29 +0100

    adding updated version of kingdom package with assignItems method completed for kingdom.class

--- exam/kingdom/Kingdom.java
+++ exam/kingdom/Kingdom.java
@@ -23,8 +23,46 @@ public class Kingdom {
     }
 
     public List<Adventurer> assignItems() {
+        List<Adventurer>  assignedAdventureres = new ArrayList<>();
 
-    }
+        for (Adventurer adventurer : fellowship) { 
+            // skip adventurers who already have an item
+            if (adventurer.hasItem()) {
+                continue;
+            }
+
+            // determine item type based on adventurer class
+            ItemType targetType = null;
+
+            if (adventurer instanceof Hobbit) {
+                targetType = ItemType.HANDHELD;
+            }
+            else if (adventurer instanceof Elf) {
+                targetType = ItemType.RANGED;
+            }
+            else if (adventurer instanceof Wizard) {
+                targetType = ItemType.MAGICAL;
+            }
+
+            if (targetType == null) {
+                continue;
+            }
+
+            // assign the first compatible item in storage
+
+            for (Item item : storage) {
+                if (item.getType() == targetType) {
+                    adventurer.assignItem(item);
+                    storage.remove(item);
+                    assignedAdventureres.add(adventurer);
+                    break; // break statement so only added is item assigned targetItem
+                }
+            }
+
+            return assignedAdventureres;
+        }
+
+    
 
     public List<Adventurer> getFellowship() {
 
