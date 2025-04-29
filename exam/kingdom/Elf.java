46472f074ec4274bca751720145e01e9eb3e6fe9
Joseph Garni <joseph.garni@student.manchester.ac.uk>
2025-04-29 14:53:15 +0100

    adding updated version of elf and hobbit subclasses to adventuter class

---
+++ exam/kingdom/Elf.java
@@ -0,0 +1,16 @@
+package kingdom;
+
+public class Elf {
+    private float accuracy;
+
+    public Elf(String name, float accuracy) {
+        this.name = name;
+        this.accuracy = accuracy;
+    }
+
+    public calculatePower() {
+    
+    float power = item.calculatePower();
+    return power * accuracy;
+    }
+}
---
+++ exam/kingdom/Hobbit.java
@@ -0,0 +1,22 @@
+package kingdom;
+
+public class Hobbit extends Adventurer {
+    private int strength;
+
+    public Hobbit(String name, int strength) {
+        this.name = name;
+        this.strength = strength;
+    }
+
+    @Override
+    public float calculatePower() {
+        Item item = getItem();
+        
+        if (item != null) {
+            float power = item.calculatePower();
+            return power * strength;
+        }
+        return 0;
+    }
+
+}
